a15988787e37828d9509358ed37acf11e8c6defb
H.Yamada <ymd_h@outlook.jp>
2025-03-20 20:45:59 +0900

    WIP: Implement org-roam-rag

--- org-roam-rag.el
+++ org-roam-rag.el
@@ -34,3 +34,94 @@
 ;;; Code:
 (require 'llm)
 (require 'org-roam)
+
+
+(defgroup org-roam-rag nil
+  "RAG over Org Roam"
+  :group 'external)
+
+(defvar org-roam-rag-llm-provider nil
+  "LLM provider for org-roam-rag.")
+
+
+(defcustom org-roam-rag-llm-system-prompt
+  "You are skillfull, kind, and friendly assistant.
+Users will ask you questions with some context documents.
+You must answer their questions based on these context documents.
+These context documents are written in emacs org-mode."
+  "System Prompt to guid LLM"
+  :type '(string)
+  :group 'org-roam-rag)
+
+
+(defcustom org-roam-rag-llm-user-prompt
+  "Please answer user question based on context documents.
+
+User Question
+-------------
+%1$s
+
+Context Documents
+-----------------
+%2$s
+"
+  "Prompt Template for RAG.
+
+User question will be inserted at %1$s,
+context documents retrieved at %2$s by `format' function"
+  :type '(string)
+  :group 'org-roam-rag)
+
+
+(defun org-roam-rag--make-llm-prompt (prompt)
+  "Make LLM prompt from PROMPT."
+  (if (fboundp 'llm-make-chat-prompt)
+      (llm-make-chat-prompt prompt :context org-roam-rag-llm-system-prompt)
+    (make-llm-chat-prompt
+     :context org-roam-rag-system-prompt
+     :interactions
+     (list (make-llm-chat-prompt-interaction :role 'user :content prompt)))))
+
+(defcustom org-roam-rag-response-buffer-name
+  "*org-roam-rag*" "Buffer name of LLM response."
+  :type '(string)
+  :group 'org-roam-rag)
+
+
+(defun org-roam-rag--response-buffer ()
+  "Create and display org-roam-rag response buffer."
+  (let ((buffer (generate-new-buffer org-roam-rag-response-buffer-name)))
+    (save-excursion
+      (with-current-buffer buffer
+        (display-buffer buffer)
+        (markdown-mode)))
+    buffer))
+
+(defun org-roam-rag--show-response-streaming (buffer response)
+  "Show LLM (partial) RESPONSE at specified BUFFER."
+  (save-excursion
+    (with-current-buffer buffer
+      (erase-buffer)
+      (insert response))))
+
+(defun org-roam-rag--chat-steaming (prompt)
+  "Chat with LLM streaming using PROMPT."
+  (let* ((buffer (org-roam-rag--response-buffer))
+         (callback #'(lambda (response)
+                       (org-roam-rag--show-response-streaming buffer response))))
+    (llm-chat-streaming org-roam-rag-llm-provider
+                        (org-roam-rag--make-llm-prompt prompt)
+                        callback callback #'ignore)))
+
+(defun org-roam-rag--retrieve (question)
+  "Retrieve documents for QUESTION")
+
+(defun org-roam-rag--ask (question)
+  "Ask QUESTION to LLM."
+  (let* ((contexts (org-roam-rag--retrieve question))
+         (prompt (format org-roam-rag-llm-user-prompt question contexts)))
+    org-roam-rag--chat-streaming prompt))
+
+
+(provide 'org-roam-rag)
+;;; org-roam-rag.el ends here
